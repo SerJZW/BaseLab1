c524c8640c2ae63935569f81bccc7e26338277da
ZemtsovSD <ZemtsovSD@313D-12>
2022-09-03 10:30:13 +0700

    Закончил первый урок

--- BaseLab1/Program.cs
+++ BaseLab1/Program.cs
@@ -1,4 +1,16 @@
 ﻿using System;
 
-      Console.WriteLine("Hello World!");
+int x;
+int y;
+Console.WriteLine("Введите значение для X");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение для Y");
+y = Convert.ToInt32(Console.ReadLine());
+int c = x;
+x = y;
+y = c;
+x = x - y;
+y = x + y;
+x = y - x;
+Console.WriteLine($"x = {x} y = {y}");
 
